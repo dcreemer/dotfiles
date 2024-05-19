ca6da27be98c85b7eed02b5a1824745cc181580b
David Creemer <david@zachary.com>
2024-05-19 04:44:53 +0000

    do not exit 0

--- dot_bash.d/brew.sh
+++ dot_bash.d/brew.sh
@@ -7,22 +7,25 @@
 # determine the brew prefix without calling the executable ("brew
 # --prefix")
 
+BREW=""
+
 for b in /opt/homebrew /usr/local /home/linuxbrew/.linuxbrew; do
     if command -v "$b"/bin/brew > /dev/null; then
         BREW="$b"
         break
     fi
     # no brew on this system
-    exit 0
 done
 
-# bash completion via Homebrew
-COMP="$BREW"/etc/profile.d/bash_completion.sh
-if [ -r "${COMP}" ]; then
-    # shellcheck source=/dev/null
-    . "${COMP}"
-fi
+if [ "$BREW" != "" ]; then
+    # bash completion via Homebrew
+    COMP="$BREW"/etc/profile.d/bash_completion.sh
+    if [ -r "${COMP}" ]; then
+        # shellcheck source=/dev/null
+        . "${COMP}"
+    fi
 
-# Turn off Homebrew analytics
-# https://github.com/Homebrew/brew/blob/master/share/doc/homebrew/Analytics.md
-export HOMEBREW_NO_ANALYTICS=1
+    # Turn off Homebrew analytics
+    # https://github.com/Homebrew/brew/blob/master/share/doc/homebrew/Analytics.md
+    export HOMEBREW_NO_ANALYTICS=1
+fi
