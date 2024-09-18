e41a19564bcb54ff9c02d23bfa6245c467d94bf2
David Creemer <david@zachary.com>
2024-09-17 19:05:43 -0700

    switch to UV for python and tools

--- dot_bash.d/python.sh
+++ dot_bash.d/python.sh
@@ -3,18 +3,24 @@
 # python
 #
 
-# use pyenv to choose and switch python interpreters
-PYENV_ROOT="$HOME/.pyenv"
-if [ -d "$PYENV_ROOT" ]; then
-    export PYENV_ROOT
-    export PATH="$PYENV_ROOT/bin:$PATH"
-fi
+# I use uv to install, choose, and run python runtimes and tools
+if command -v uv 1>/dev/null 2>&1; then
+    alias python="uv run python3"
+    alias python3="uv run python3"
+else
+    # check for (older) pyenv and pipx
+    PYENV_ROOT="$HOME/.pyenv"
+    if [ -d "$PYENV_ROOT" ]; then
+        export PYENV_ROOT
+        export PATH="$PYENV_ROOT/bin:$PATH"
+    fi
 
-if command -v pyenv 1>/dev/null 2>&1; then
-    eval "$(pyenv init --path)"
-    eval "$(pyenv init -)"
-fi
+    if command -v pyenv 1>/dev/null 2>&1; then
+        eval "$(pyenv init --path)"
+        eval "$(pyenv init -)"
+    fi
 
-if command -v pipx 1>/dev/null 2>&1; then
-    eval "$(register-python-argcomplete pipx)"
+    if command -v pipx 1>/dev/null 2>&1; then
+        eval "$(register-python-argcomplete pipx)"
+    fi
 fi
