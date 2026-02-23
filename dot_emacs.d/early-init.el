eaaca7c675d3337337eadf2238d27723f038bffd
David Creemer <david@zachary.com>
2026-02-23 19:46:11 +0100

    terminal model tweak

--- dot_emacs.d/early-init.el
+++ dot_emacs.d/early-init.el
@@ -9,11 +9,11 @@
 
 ;; turn on menu (for GUIs only), turn off tool-bar, scroll-bar
 (if (display-graphic-p)
-    (menu-bar-mode 1)
+    (progn
+      (menu-bar-mode 1)
+      (tool-bar-mode -1)
+      (scroll-bar-mode -1))
   (menu-bar-mode -1))
-
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
 
 ;; So we can detect this having been loaded
 (provide 'early-init)
